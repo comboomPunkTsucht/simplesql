17e6a70c95f627dab5e53f86026a44a562fe6b51
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-06-05 18:09:55 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@
 //! Clone the repository and build it using Cargo:
 //!
 //! ```bash
-//! git clone https://github.com/your-username/simplesql.git
+//! git clone https://github.com/comboomPunkTsucht/simplesql.git
 //! cd simplesql
 //! rustup target add aarch64-apple-darwin aarch64-unknown-linux-gnu aarch64-unknown-linux-musl aarch64-pc-windows-msvc x86_64-apple-darwin x86_64-pc-windows-msvc x86_64-unknown-freebsd x86_64-unknown-linux-gnu x86_64-unknown-linux-musl
 //! cargo build --release

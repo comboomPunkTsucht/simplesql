b78829ac6e13294f388e80b37fb82115c3db2878
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-06-24 22:00:52 +0200

    .
    Signed-off-by: mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@
 //! ```bash
 //! git clone https://github.com/comboomPunkTsucht/simplesql.git
 //! cd simplesql
-//! rustup target add aarch64-apple-darwin aarch64-unknown-linux-gnu aarch64-unknown-linux-musl aarch64-pc-windows-msvc x86_64-apple-darwin x86_64-pc-windows-msvc x86_64-unknown-freebsd x86_64-unknown-linux-gnu x86_64-unknown-linux-musl
+//! rustup target add aarch64-apple-darwin aarch64-unknown-linux-gnu aarch64-unknown-linux-musl aarch64-pc-windows-msvc aarch64-pc-windows-gnullvm x86_64-apple-darwin x86_64-pc-windows-msvc x86_64-unknown-freebsd x86_64-unknown-linux-gnu x86_64-unknown-linux-musl x86_64-pc-windows-gnu x86_64-pc-windows-gnullvm x86_64-pc-windows-gnu x86_64-pc-windows-gnu
 //! cargo build --release
 //! ```
 //!
